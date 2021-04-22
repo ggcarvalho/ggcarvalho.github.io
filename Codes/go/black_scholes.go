b5cc8f0ccab9e0003cc03147373c11ea279f6cb4
Gabriel Guimaraes Carvalho <ggc5@motorola.com>
2021-04-21 22:47:24 -0300

    add black-scholes section

--- Codes/go/black_scholes.go
+++ Codes/go/black_scholes.go
@@ -1,23 +1,59 @@
-// import math
-// from numpy import *
-// from time import time
-// # star import for shorter code
-// random.seed(20000)
-// t0 = time()
-// # Parameters
-// S0 = 100.; K = 105.; T = 1.0; r = 0.05; sigma = 0.2
-// M = 50; dt = T / M; I = 250000
-// # Simulating I paths with M time steps
-// S = S0 * exp(cumsum((r - 0.5 * sigma ** 2) * dt
-// + sigma * math.sqrt(dt)
-// * random.standard_normal((M + 1, I)), axis=0))
-// # sum instead of cumsum would also do
-// # if only the final values are of interest
-// S[0] = S0
-// # Calculating the Monte Carlo estimator
-// C0 = math.exp(-r * T) * sum(maximum(S[-1] - K, 0)) / I
-// # Results output
-// tnp2 = time() - t0
+package main
 
-// print('The European Option Value is: ', C0)  # The European Option Value is:  8.165807966259603
-// print('The Execution Time is: ',tnp2)  # The Execution Time is:  0.9024488925933838
+import (
+	"fmt"
+	"time"
+	"math"
+	"math/rand"
+)
+
+
+func relu(x float64) float64 {
+	if x >= 0.0 {
+		return x
+	}
+	return 0.0
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	start := time.Now()
+
+	// Parameters
+	S0 := 100.0 // initial value
+	K := 105.0 // strike price
+	T := 1.0 // maturity
+	r := 0.05 //risk free short rate
+	sigma := 0.2 //volatility
+	M := 50 // number of time steps
+	dt := T / float64(M) //length of time interval
+	I := 250000 // number of paths
+
+	var S [][]float64
+
+	// Simulating I paths with M time steps
+	for i := 1; i < I; i++ {
+		var path []float64
+		for t := 0; t <= M; t++ {
+			if t == 0 {
+				path = append(path, S0)
+			} else {
+				z := rand.NormFloat64()
+				St := path[t - 1]*math.Exp((r - 0.5*(sigma*sigma))*dt + sigma*math.Sqrt(dt)*z)
+				path = append(path, St)
+			}
+		}
+		S = append(S, path)
+	}
+
+	// Calculating the Monte Carlo estimator
+	sum_val := 0.0
+	for _,p := range S {
+		sum_val += relu(p[len(p) - 1] - K)
+	}
+	C0 := math.Exp(-r*T)*sum_val / float64(I)
+
+	duration := time.Since(start)
+	fmt.Printf("European Option Value %.3f\n", C0)
+	fmt.Println("Execution time: ", duration)
+}
